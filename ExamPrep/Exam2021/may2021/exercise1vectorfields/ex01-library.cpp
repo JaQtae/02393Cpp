77ac3b2284986001e06b296cda9f0f3fee64fe0f
JaQtae <s194264@student.dtu.dk>
2024-12-21 07:32:42 +0100

    All exam solutions. Clean up. Finalizing before exam

--- ExamPrep/Exam2021/may2021/exercise1vectorfields/ex01-library.cpp
+++ ExamPrep/Exam2021/may2021/exercise1vectorfields/ex01-library.cpp
@@ -6,24 +6,55 @@ using namespace std;
 // Task 1(a).  Implement this function
 Vector **createField(unsigned int m, unsigned int n, Vector v) {
     // Write your code here
-	return nullptr;
+    Vector** A = new Vector*[m]; // Allocate memory
+    // init each array element as arg v
+    for (unsigned int i=0; i<m; i++) {
+        A[i] = new Vector[n]; // Initialize each row
+        for (unsigned int j=0; j<n; j++) {
+            A[i][j] = v; // Set each one to v
+        }
+    }
+	return A; // return vectorfield.
 }
 
 // Task 1(b).  Implement this function
 void displayField(Vector **A, unsigned int m, unsigned int n) {
     // Write your code here
+    for (unsigned int i=0; i<m; i++) {
+        for (unsigned int j=0; j<n; j++) {
+            if (j==n-1) { // we're in the last column, so no space.
+                cout << "(" << A[i][j].x <<"," << A[i][j].y << ")";
+            } else {
+                cout << "(" << A[i][j].x <<"," << A[i][j].y << ")" << " ";
+            }
+
+        }
+    cout << endl; // New line per row
+    }
 }
 
 // Task 1(c).  Implement this function
 void addFields(Vector **A, Vector **B, Vector **C,
                unsigned int m, unsigned int n) {
     // Write your code here
+    for (unsigned int i=0; i<m; i++) {
+        for (unsigned int j=0; j<n; j++) {
+            C[i][j].x = A[i][j].x + B[i][j].x; // Addition
+            C[i][j].y = A[i][j].y + B[i][j].y;
+        }
+    } 
 }
 
 // Task 1(d).  Implement this function
 void scaleField(Vector **A, double c,
                 unsigned int m, unsigned int n) {
     // Write your code here
+        for (unsigned int i=0; i<m; i++) {
+        for (unsigned int j=0; j<n; j++) {
+            A[i][j].x = A[i][j].x * c; // Scale by multiplying
+            A[i][j].y = A[i][j].y * c;
+        }
+    } 
 }
 
 // Do not modify
--- ExamPrep/Exam2021/may2021/exercise2rlelinkedlists/ex02-library.cpp
+++ ExamPrep/Exam2021/may2021/exercise2rlelinkedlists/ex02-library.cpp
@@ -5,18 +5,65 @@ using namespace std;
 // Task 2(a).  Implement this function
 Elem* reverse(Elem *list) {
     // Write your code here
-	return nullptr;
+    // Init head of linked list
+    Elem* head = list;
+    // Empty or one element
+    if (head == nullptr || head->next == nullptr) {
+        return head;
+    }
+    // (1)->(2)->(3)->(4)->(5)->()
+    // => (head)->(rest) [Rest is 2<-3<-4<-5]
+    // Recursively go through each node and reverse the pointer
+    Elem* rest = reverse(head->next);
+    // => (head)->()
+    // Remove link between (head) and (rest)
+    head->next->next = head;
+    // head is the new end of the list
+    head->next = NULL;
+    // Return rest
+    return rest;
+
 }
 
 // Task 2(b).  Implement this function
 Elem* concatenate(Elem *list1, Elem *list2) {
     // Write your code here
-	return nullptr;
+
+    // If list1 is empty, return list2
+    if (list1 == nullptr) {
+        return list2;
+    }
+    // If list2 is empty, return list1
+    if (list2 == nullptr) {
+        return list1;
+    }
+
+    // Traverse to the end of list1
+    Elem* last = list1;
+    while (last->next != nullptr) {
+        last = last->next;
+    }
+    // If last elem in list1 is same as list2
+    if (last->value == list2->value) {
+        last->times += list2->times; // merge TIMES of elements
+        last->next = list2->next; // point to next element
+    } else {
+        last->next = list2; // attach list2 to end of list1
+    }
+    return list1;
 }
 
 // Task 2(c).  Implement this function
 int sum(Elem *list) {
     // Write your code here
+    for (auto i = list; i != nullptr; i = i->next) {
+        // If list is empty, return 0
+        if (list == nullptr) {
+            return 0;
+        }
+        // Sum X times of value + sum X times of next element, etc.
+        return i->value * i->times + sum(i->next);
+    }
 	return 0;
 }
 
